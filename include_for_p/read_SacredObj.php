8f7caa495dac1275aa7c3d4011d12f499d0f840c
Mr.Billy <80222544+jirapat252262120@users.noreply.github.com>
2021-04-04 20:31:04 +0700

    ก่อนย้ายไปทำโน๊ตบุ๊ค

--- include_for_p/read_SacredObj.php
+++ include_for_p/read_SacredObj.php
@@ -5,38 +5,45 @@
                 $readSacredObj = new shopSacredObj();
                 $id4readSacredObj = $_GET['id4readSacredObj'];
                 
-                $sql = readSacredObj->runQuery("SELECT * FROM product WHERE id_product");        
+                $sql = $readSacredObj->runQuery("SELECT * FROM product WHERE id_product='$id4readSacredObj' ");
+                if($sql){
+                    $fetchData = mysqli_fetch_array($sql);
+                }else{
+                    ?>
+            <script>
+            alert("Can't SQL");
+            </script>
+            <?php
+                }        
 
                 ?>
-            
+
             <div class="row">
                 <div class="col-md-7">
                     <div class="card w-100 p-3">
-                        <img src="https://cdn.pixabay.com/photo/2016/11/03/04/02/boys-1793421_960_720.jpg" alt="">
+                        <img src="image/product/<?php echo $fetchData['product_cover']; ?>" alt="">
                     </div>
                 </div>
                 <div class="col-md">
                     <div class="w-100 p-0 pl-2 ">
                         <strong>
-                            <h1 class="pt-3">
-                                ชื่อสินค้า
-                            </h1>
+                            <h2 id="name_shop_card" class="pt-3 font-weight-bold">
+                                <?php echo $fetchData['product_name']; ?>
+                            </h2>
                         </strong>
                         <p class="m-0">
-                            ประเภทสินค้า
+                            ประเภทวัตถุมงคล : <?php echo $fetchData['product_type'];?>
                         </p>
                         <div class="m-0 d-flex d-inline ">
-                            ราคา <p class="m-0 mr-2 ml-2 text-danger font-weight-bold"><b>500</b> </p>บาท
+                            ราคา <p class="m-0 mr-2 ml-2 text-danger font-weight-bold">
+                                <b><?php echo $fetchData['product_price'];?></b>
+                            </p>บาท
                         </div>
                         <div class="d-flex d-inline">
-                            <p class="font-weight-bold mr-2">คงเหลือ</p> 9 ชิ้น
+                            <p class="font-weight-bold mr-2">คงเหลือ</p> <?php echo $fetchData['product_qty'];?> ชิ้น
                         </div>
                         <div>
-                            <p id="detailSacredObj">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam
-                                nobis, a voluptate nemo eius eos? Et dolores provident rem ullam dolorem. Consectetur
-                                ipsum corrupti perferendis fugit dolores adipisci, ad sed temporibus. Cum, vero placeat
-                                eos exercitationem voluptate autem molestias velit nostrum facilis expedita porro error
-                                sint possimus quos necessitatibus accusamus!</p>
+                            <p id="detailSacredObj"><?php echo $fetchData['product_des'];?></p>
                         </div>
 
                         <div class="d-flex d-inline justify-content-between align-items-center">
@@ -46,11 +53,20 @@
                                 <div for="">ชิ้น</div>
                             </div>
                             <div class="text-right">
-                            <button class="btn btn-success">
-                            <i class="fas fa-cart-arrow-down"></i> เพิ่มลงตะกร้าสินค้า
-                            </button>
+                                <button class="btn btn-success">
+                                    <i class="fas fa-cart-arrow-down"></i> เพิ่มลงตะกร้าสินค้า
+                                </button>
                             </div>
                         </div>
+                        <p>
+                       
+            
+                        </p>
+
+
+
+
+
 
                     </div>
                 </div>
